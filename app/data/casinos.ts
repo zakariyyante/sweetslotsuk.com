0cdb428793f96895db68b07e35371bb2245fcc86
Javid Mamadli <javid.mammadli99@gmail.com>
2026-02-19 13:12:31 +0400

    ..

--- app/data/casinos.ts
+++ app/data/casinos.ts
@@ -32,47 +32,47 @@ const topBadges = ["Best Bonus", "Top Pick", "Trending Now"];
 
 // Raw casino data without id, rating, and badgessss
 const casinosRawData: CasinoRawData[] = [
-  // {
-  //   name: "VegasHero",
-  //   logo: "/VegasHero.png",
-  //   bonus: "125% up to £1,250 + 250 Free Spins",
-  //   url: "https://topbritcasinos.com/B7W2b6jr?utm_target=vegas",
-  //   votes: 6440,
-  //   isMobile: true
-  // },
-  // {
-  //   name: "BetNinja",
-  //   logo: "/BetNinja.png",
-  //   bonus: "£1000 + 100 Free Spins",
-  //   url: "https://topbritcasinos.com/B7W2b6jr?utm_target=betninja",
-  //   votes: 4915,
-  //   isMobile: true
-  // },
-  // {
-  //   name: "Qbet",
-  //   logo: "/qbet.svg",
-  //   bonus: "100% UP TO £100 BONUS + 200 FREE SPINS",
-  //   url: "https://topbritcasinos.com/B7W2b6jr?utm_target=qbet",
-  //   votes: 3986,
-  //   isMobile: true
-  // },
-  // {
-  //   name: "SpinShark",
-  //   logo: "/spinshark.svg",
-  //   bonus: "£900 BONUS + 150 Free Spins",
-  //   url: "https://topbritcasinos.com/B7W2b6jr?utm_target=spinshark",
-  //   votes: 4143,
-  //   isMobile: true
-  // },
-  // {
-  //   name: "Lizaro",
-  //   logo: "/lizaro.png",
-  //   // bonus: "350% up to £680 + 200 FREE SPINS",
-  //   bonus: "200% up to £1000 + 250 FREE SPINS",
-  //   url: "https://topbritcasinos.com/B7W2b6jr?utm_target=lizaro",
-  //   votes: 5872,
-  //   isMobile: true
-  // },
+  {
+    name: "BetNinja",
+    logo: "/BetNinja.png",
+    bonus: "£1000 + 100 Free Spins",
+    url: "https://topbritcasinos.com/B7W2b6jr?utm_target=betninja",
+    votes: 4915,
+    isMobile: true
+  },
+  {
+    name: "SpinShark",
+    logo: "/spinshark.svg",
+    bonus: "£900 BONUS + 150 Free Spins",
+    url: "https://topbritcasinos.com/B7W2b6jr?utm_target=spinshark",
+    votes: 4143,
+    isMobile: true
+  },
+  {
+    name: "Qbet",
+    logo: "/qbet.svg",
+    bonus: "100% UP TO £100 BONUS + 200 FREE SPINS",
+    url: "https://topbritcasinos.com/B7W2b6jr?utm_target=qbet",
+    votes: 3986,
+    isMobile: true
+  },
+  {
+    name: "HarryCasino",
+    logo: "/harry.svg",
+    bonus: "£1000 BONUS + 100 Free Spins",
+    url: "https://topbritcasinos.com/B7W2b6jr?utm_target=harry",
+    votes: 3951,
+    isMobile:true
+  },
+  {
+    name: "Lizaro",
+    logo: "/lizaro.png",
+    // bonus: "350% up to £680 + 200 FREE SPINS",
+    bonus: "200% up to £1000 + 250 FREE SPINS",
+    url: "https://topbritcasinos.com/B7W2b6jr?utm_target=lizaro",
+    votes: 5872,
+    isMobile: true
+  },
   // {
   //   name: "SpinFin",
   //   logo: "/spinfin.svg",
@@ -105,16 +105,17 @@ const casinosRawData: CasinoRawData[] = [
 
 
 
-
-
   // {
-  //   name: "HarryCasino",
-  //   logo: "/harry.svg",
-  //   bonus: "£1000 BONUS + 100 Free Spins",
-  //   url: "https://topbritcasinos.com/B7W2b6jr?utm_target=harry",
-  //   votes: 3951,
-  //   isMobile:true
+  //   name: "VegasHero",
+  //   logo: "/VegasHero.png",
+  //   bonus: "125% up to £1,250 + 250 Free Spins",
+  //   url: "https://topbritcasinos.com/B7W2b6jr?utm_target=vegas",
+  //   votes: 6440,
+  //   isMobile: true
   // },
+  //
+
+
  
   //
   // {
