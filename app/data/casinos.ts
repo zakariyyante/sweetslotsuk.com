d04f3ba00916217d3089b74a0eef4909a1f86ae6
Zakariyya <zakariyya.z@antetechnologies.com>
2026-04-27 19:35:46 +0400

    westace

--- app/data/casinos.ts
+++ app/data/casinos.ts
@@ -33,6 +33,14 @@ const topBadges = ["Best Bonus", "Top Pick", "Trending Now"];
 // Raw casino data without id, rating, and badgessss
 const casinosRawData: CasinoRawData[] = [
   {
+      name: "Westace",
+      logo: "/westace.webp",
+      bonus: "300% UP TO €2000 + 150 FS",
+      url: "https://westace.muforaset.live?sub_id_1=t4p6a0wbej43uo&utm_source=westace",
+      votes: 5872,
+      isMobile: true
+    },
+  {
     name: "DaytonaSpin",
     logo: "/daytonaspin.svg",
     bonus: "255% UP TO £4500 + 255 FREE SPINS",
@@ -50,14 +58,7 @@ const casinosRawData: CasinoRawData[] = [
   // },
   
   // //
-  // {
-  //   name: "Westace",
-  //   logo: "/westace.webp",
-  //   bonus: "300% UP TO €2000 + 150 FS",
-  //   url: "https://westace.muforaset.live?sub_id_1=t4p6a0wbej43uo&utm_source=westace",
-  //   votes: 5872,
-  //   isMobile: true
-  // },
+  // 
   
   // //
   // // //
