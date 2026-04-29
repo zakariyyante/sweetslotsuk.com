4ee4a1466e5ad3638573d028474d37c8072ee9a3
Zakariyya <zakariyya.z@antetechnologies.com>
2026-04-29 17:58:20 +0400

    sankra

--- app/data/casinos.ts
+++ app/data/casinos.ts
@@ -118,14 +118,14 @@ const casinosRawData: CasinoRawData[] = [
   //   isMobile: true
   // },
   
-  // {
-  //   name: "Sankra",
-  //   logo: "/sankra.svg",
-  //   bonus: "100% UP TO £600 + 200 FREE SPINS",
-  //   url: "https://sankra.muforaset.live?sub_id_1=t4p6a0wbej43uo&utm_source=sankra",
-  //   votes: 5872,
-  //   isMobile: true
-  // },
+  {
+    name: "Sankra",
+    logo: "/sankra.svg",
+    bonus: "100% UP TO £600 + 200 FREE SPINS",
+    url: "https://sankra.muforaset.live?sub_id_1=t4p6a0wbej43uo&utm_source=sankra",
+    votes: 5872,
+    isMobile: true
+  },
 
 
 
