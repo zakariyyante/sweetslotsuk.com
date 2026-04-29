ea370ab325adacd6242af2d15802f2d0c031146c
Zakariyya <zakariyya.z@antetechnologies.com>
2026-04-29 17:28:27 +0400

    daytona pause1

--- app/data/casinos.ts
+++ app/data/casinos.ts
@@ -40,23 +40,6 @@ const casinosRawData: CasinoRawData[] = [
     votes: 5872,
     isMobile: true
   },
-  
-  {
-    name: "Westace",
-    logo: "/westace.webp",
-    bonus: "300% UP TO €2000 + 150 FREE SPINS",
-    url: "https://westace.muforaset.live?sub_id_1=t4p6a0wbej43uo&utm_source=westace",
-    votes: 5872,
-    isMobile: true
-  },
-  // {
-  //   name: "DaytonaSpin",
-  //   logo: "/daytonaspin.svg",
-  //   bonus: "255% UP TO £4500 + 255 FREE SPINS",
-  //   url: "https://daytona.muforaset.live?sub_id_1=t4p6a0wbej43uo&utm_source=daytona",
-  //   votes: 5872,
-  //   isMobile: true
-  // },
   {
     name: "Spinboss",
     logo: "/spinboss.webp",
@@ -65,6 +48,24 @@ const casinosRawData: CasinoRawData[] = [
     votes: 5872,
     isMobile: true
   },
+  {
+    name: "Westace",
+    logo: "/westace.webp",
+    bonus: "300% UP TO €2000 + 150 FREE SPINS",
+    url: "https://westace.muforaset.live?sub_id_1=t4p6a0wbej43uo&utm_source=westace",
+    votes: 5872,
+    isMobile: true
+  },
+  
+  // {
+  //   name: "DaytonaSpin",
+  //   logo: "/daytonaspin.svg",
+  //   bonus: "255% UP TO £4500 + 255 FREE SPINS",
+  //   url: "https://daytona.muforaset.live?sub_id_1=t4p6a0wbej43uo&utm_source=daytona",
+  //   votes: 5872,
+  //   isMobile: true
+  // },
+  
   
   
   
